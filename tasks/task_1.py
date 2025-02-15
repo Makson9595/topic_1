0774d6a1002677715488e9676cf398598a89356a
Bill Williamson <vkontakte-maksimka@mail.ru>
2025-02-15 13:35:24 +0300

    Домашнее задание
    - Решение задач: №1

--- tasks/task_1.py
+++ tasks/task_1.py
@@ -1,3 +1,3 @@
-print('Hello, World!')  # Одинарные кавычки
-print("Hello, World!")  # Двойные кавычки
-print('''Hello, World!''')  # Тройные кавычки
+print ('Hello, World!')
+print ("Hello, World!")
+print ("""Hello, World!""")
